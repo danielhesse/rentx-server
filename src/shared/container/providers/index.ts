baac255eebbb0cca4ead8d671162f524bb8c1771
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-06 15:42:07 -0300

    deploy: s3 upload

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import { DayJsDateprovider } from './DateProvider/implementations/DayJsDateprovi
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>('DateProvider', DayJsDateprovider);
@@ -14,7 +15,12 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider(),
 );
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  LocalStorageProvider,
+  diskStorage[process.env.DISK],
 );
