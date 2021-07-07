53729dc6f3cb8b23b803d03c3c872564cd49ab39
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-07 13:00:52 -0300

    deploy: rate limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,12 +10,15 @@ import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
+import rateLimiter from './middlewares/rateLimiter';
 import { routes } from './routes';
 
 import '@shared/container';
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
