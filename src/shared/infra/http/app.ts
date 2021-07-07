8704fd6c49a2d5f51cda81365d5aa348bae98625
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-07 13:19:40 -0300

    deploy: sentry config

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import upload from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 
@@ -20,6 +22,20 @@ const app = express();
 
 app.use(rateLimiter);
 
+if (process.env.ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+}
+
 app.use(express.json());
 
 // Documentation
@@ -30,6 +46,8 @@ app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
