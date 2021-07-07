a23f56b88c8e617228631ab62a63326d84f27879
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-07 09:51:16 -0300

    Production Configutation

--- dist/shared/container/providers/DateProvider/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DayJsDateprovider = require("./implementations/DayJsDateprovider");
-
-_tsyringe.container.registerSingleton('DateProvider', _DayJsDateprovider.DayJsDateprovider);
--- dist/shared/infra/http/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _app = require("./app");
-
-_app.app.listen(3333, () => {
-  console.log('🚀 Server running on port 3333.');
-});
